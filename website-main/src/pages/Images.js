48df8974ccf2ac3566b85ff6267663899143976c
shubh patni <shubhpatni2002@gmail.com>
2022-02-20 17:30:25 -0500

    oops

--- website-main/src/pages/Images.js
+++ website-main/src/pages/Images.js
@@ -1,4 +1,9 @@
-// import * as React from "react";
+import * as React from "react";
+
+export default function Images() {
+  return <h1> Need to work on this</h1>;
+}
+
 // import Navigation from "../components/Navigation";
 // import "../styles/gallary.css";
 // import { useStaticQuery, graphql } from "gatsby";
